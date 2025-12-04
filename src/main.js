9154ab7909328e46388964b08740ea563c30f6a4
Casey Horton <cjh1295@gmail.com>
2025-12-04 18:35:05 -0500

    Finished Video - Integrating with Other Languages

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const {exec} = require('child_process');
+
 // argument variables
 const args = process.argv.slice(2);
 const command = args[0];
@@ -15,7 +17,51 @@ function displayUsage() {
 
 // open function
 function open(favorite) {
-    console.log(favorite);
+
+    // generate the url based on the input
+    let url;
+    if (favorite === 'goog') {
+        url = 'https://google.com'
+    } else if (favorite === 'social') {
+        url = 'https://instagram.com';
+    } else if (favorite === 'code') {
+        url = 'https://leetcode.com'
+    } else {
+        console.log('shortcut', favorite, 'does not exist');
+        return;
+    }
+
+    // platform independant command generation
+    let command;
+        switch (process.platform) {
+        case 'darwin' :
+            command = `open -a Google Chrome ${url}`;
+            break;
+        case 'win32' :
+            command = `start chrome ${url}`;
+            break;
+        case 'linux':
+            command = `google-chrome ${url}`;
+            break;
+        default:
+            console.log('Unsupported Platform');
+    }
+    
+    // execute the command
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log('error:', error.message);
+        }
+        if (stderr) {
+            console.log('error:', stderr);
+        }
+        if (error || stderr) {
+            return;
+        }
+        console.log(stdout);
+    });
+
+    console.log('Opening', url);
 }
 
 // add function
